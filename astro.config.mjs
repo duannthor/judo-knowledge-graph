35f9080cae95e65aa734522f7977c06a692aab76
Thor Duann <duannthor@gmail.com>
2026-05-26 13:27:30 -0400

    fix url links

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   output: "static",
   site: "https://thorduann.github.io/judo-knowledge-graph",
-  base: "/judo-knowledge-graph"
+  base: "/judo-knowledge-graph/"
 });
